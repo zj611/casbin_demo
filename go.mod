bdd62798aa88cd8a238bfd2d13717079fd84d3c0
jun <zhangjun233@sfmail.sf-express.com>
2021-10-29 18:03:27 +0800

    update

--- go.mod
+++ go.mod
@@ -3,10 +3,15 @@ module casbin
 go 1.15
 
 require (
+	github.com/billcobbler/casbin-redis-watcher/v2 v2.0.0-20211014141847-d29c8619193d
 	github.com/casbin/casbin/v2 v2.37.4
-	github.com/casbin/gorm-adapter/v2 v2.1.0 // indirect
+	github.com/casbin/gorm-adapter/v2 v2.1.0
 	github.com/casbin/gorm-adapter/v3 v3.4.4
+	github.com/casbin/redis-watcher/v2 v2.0.0-20211014085820-755034fcd60d
+	github.com/go-redis/redis/v8 v8.8.0
 	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/mlsen/casbin-redis-adapter/v2 v2.0.1 // indirect
+	github.com/rafaeljusto/redigomock v2.4.0+incompatible // indirect
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.15
 )
--- main.go
+++ main.go
@@ -4,12 +4,48 @@ import (
 	"fmt"
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
+	//"github.com/mlsen/casbin-redis-adapter/v2"
+
 	"gorm.io/driver/mysql"
 	//"github.com/jinzhu/gorm"
 	"gorm.io/gorm"
 )
 
 func main() {
+	//re_a, err := redisadapter.NewFromURL("redis://:123@localhost:6380/0")
+	//if err != nil{
+	//	panic(err)
+	//}
+	//// Initialize a new Enforcer with the redis adapter
+	//re, err := casbin.NewEnforcer("model.conf", re_a)
+	//if err != nil{
+	//	panic(err)
+	//}
+	//re.EnableAutoSave(true)
+	//
+	//// Load policy from redis
+	//re.LoadPolicy()
+	//
+	//// Add a policy to redis
+	//re.AddPolicy("alice", "data", "read")
+	//
+	//// Check for permissions
+	//isok, _ := re.Enforce("alice", "data", "read")
+	//fmt.Println(isok)
+	//// Delete a policy from redis
+	//re.RemovePolicy("alice", "data1", "read")
+	//
+	//// Save all policies to redis
+	//
+	//b, err := re.AddNamedPolicy("p", "12role::1", "stra::1", "on")
+	//fmt.Println(b,err)
+
+	//watcher
+	//w, err := watcher.NewWatcher("127.0.0.1:6380",watcher.Password("123"))
+	//if err != nil {
+	//	panic(err)
+	//}
+
 	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:                       "root:root@tcp(0.0.0.0:3306)/test?charset=utf8mb4&parseTime=True&loc=Local", // data source name, refer https://github.com/go-sql-driver/mysql#dsn-data-source-name
 		DefaultStringSize:         256,                                                                         // add default size for string fields, by default, will use db type `longtext` for fields without size, not a primary key, no index defined and don't have default values
@@ -26,6 +62,13 @@ func main() {
 	// The adapter will use an existing gorm.DB instnace.
 	a, _ := gormadapter.NewAdapterByDBWithCustomTable(db, &CasbinRulePre{})
 	e, _ := casbin.NewEnforcer("model.conf", a)
+
+	if err != nil {
+		panic(err)
+	}
+	e.EnableAutoNotifyWatcher(true)
+
+	//e, _ := casbin.NewDistributedEnforcer("model.conf", a,re_a)
 	e.EnableAutoSave(true)
 
 	// 向当前命名策略添加授权规则。 如果规则已经存在，函数返回false，并且不会添加规则。 否则，函数通过添加新规则并返回true
@@ -37,9 +80,8 @@ func main() {
 
 	e.AddNamedPolicy("p", "apikey::100", "stra::1", "on")
 
-
 	//将命名角色继承规则添加到当前策略。 如果规则已经存在，函数返回false，并且不会添加规则。 否则，函数通过添加新规则并返回true
-	e.AddNamedGroupingPolicy("g", "admin::1", "role::111")
+	e.AddNamedGroupingPolicy("g", "apikey::88", "role::root")
 	e.AddNamedGroupingPolicy("g", "apikey::1", "role::1")
 	e.AddNamedGroupingPolicy("g", "apikey::11", "role::1")
 	e.AddNamedGroupingPolicy("g", "apikey::2", "role::2")
@@ -47,21 +89,55 @@ func main() {
 	e.AddNamedGroupingPolicy("g2", "uriid::1", "svcg::1")
 	e.AddNamedGroupingPolicy("g2", "uriid::2", "svcg::2")
 
+	e.AddNamedGroupingPolicy("g2", "uriid::2", "stra::1")
 	e.AddNamedGroupingPolicy("g2", "uriid::1", "stra::1")
 	e.AddNamedGroupingPolicy("g2", "uriid::11", "stra::1")
 	e.AddNamedGroupingPolicy("g2", "uriid::2", "stra::2")
+
+	//e.AddNamedGroupingPolicy("g3", "apikey::1", "user::1")
+	//e.AddNamedGroupingPolicy("g3", "apikey::2", "user::1")
 
 	// Load the policy from DB.
 	e.LoadPolicy()
 
 	// Modify the policy.
-	//fmt.Println(e.GetUsersForRole("role::1"))
-	//fmt.Println(e.GetUsersForRole("stra::1"))
-	//fmt.Println(e.GetImplicitUsersForRole("stra::1"))
-	//fmt.Println(e.EnforceEx())
-	check1(e, "apikey::100", "stra::1", "on")
-	check1(e, "admin::1", "svcg::31212", "on")
-	check1(e, "apikey::1", "svcg::1", "on")
+
+	//=====================判断是否超管角色 "admin::1"====================
+	//fmt.Println(e.HasNamedGroupingPolicy("g","admin::1","role::111"))
+	//===========================================================
+
+	//=====================根据"apikey::1"找到服务组id====================
+	//d1, _ := e.GetImplicitPermissionsForUser("apikey::1")
+	//fmt.Println(d1)
+	//===========================================================
+
+	//=====================根据"apikey::1"找到服务组id,再查到uri id====================
+	//d, _ := e.GetImplicitPermissionsForUser("apikey::1")
+	////[[role::1 stra::1 on] [role::1 stra::2 on] [role::1 svcg::1 on]]
+	//m := make(map[string]int)
+	//for _,v := range d{
+	//	//fmt.Println(v[1])
+	//	dd := e.GetFilteredNamedGroupingPolicy("g2",1,v[1])
+	//	for _,v1 := range dd{
+	//		m[v1[0]]++
+	//	}
+	//}
+	//for k,_ := range m{
+	//	t := strings.Split(k,"::")[1]
+	//	fmt.Println("t",t)
+	//}
+	//===========================================================
+	//6485221843148800
+
+	//check1(e, "apikey::100", "stra::1", "on")
+	//p1 := []string{ "apikey::100", "stra::1", "on"}
+	//p2 := []string{"apikey::100", "stra::1", "off"}
+	//e.UpdateNamedPolicy("p",p1, p2)
+	//check1(e, "apikey::100", "stra::1", "on")
+
+	fmt.Println("iiiii")
+	//check1(e, "admin::1", "svcg::31212", "on")
+	//check1(e, "apikey::1", "svcg::1", "on")
 	//=====================用户查询角色 "apikey::1"====================
 	//fmt.Println(e.GetRolesForUser("apikey::1"))
 	//===========================================================
@@ -70,19 +146,20 @@ func main() {
 	//fmt.Println(e.GetImplicitPermissionsForUser("apikey::1")) //[[role::1 stra::1 on] [role::1 stra::2 on] [role::1 svcg::1 on]]
 	//===========================================================
 
+	//=====================根据"group::1"找到uri::1====================
+	fmt.Println(e.GetFilteredNamedGroupingPolicy("g2", 1, "stra::1"))
+	//===========================================================
 
 	//=====================根据"role::1"找到apikey====================
 	//fmt.Println(e.GetUsersForRole("role::1")) //[[role::1 stra::1 on] [role::1 stra::2 on] [role::1 svcg::1 on]]
 	//===========================================================
-
-
 
 	//=====================删除单一的用户-角色 "apikey::1"====================
 	//e.DeleteRoleForUser("apikey::2", "role::2")
 	//===========================================================
 
 	//=====================删除角色"role::1"====================
-	//e.DeleteRole("role::1")//一次性全部删除权限关系,和归属关系
+	//e.DeleteRole("role::2")//一次性全部删除权限关系,和归属关系
 	//===========================================================
 
 	//=====================删除用户"apikey::1"====================
